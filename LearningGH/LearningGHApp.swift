58948de360f8831d6a0441605f07da45cba5520f
KenCJHuang <kencjhuang@gmail.com>
2021-07-17 09:04:42 +0800

    Initial Commit

---
+++ LearningGH/LearningGHApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningGHApp.swift
+//  LearningGH
+//
+//  Created by 黃俊榮 on 2021/7/17.
+//
+
+import SwiftUI
+
+@main
+struct LearningGHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
